2932eb663dab4f9a9dec8dc5f12b94978eb6d72f
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2022-09-08 21:35:06 +0200

    Add From/To event golang tests

--- golang/go.mod
+++ golang/go.mod
@@ -4,5 +4,12 @@ go 1.18
 
 require (
 	github.com/golang/protobuf v1.5.2
+	github.com/stretchr/testify v1.8.0
 	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ golang/pkg/events/events_test.go
@@ -0,0 +1,100 @@
+package events_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/trento-project/contracts/pkg/events"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+func TestToEvent(t *testing.T) {
+	event := events.FactsGatheringRequested{
+		ExecutionId: "exe",
+		GroupId:     "group_1",
+		Targets:     []string{"target1"},
+		Facts: []*events.AgentFact{
+			{
+				AgentId: "agent_1",
+				Facts: []*events.FactDefinition{
+					{
+						CheckId:  "check_1",
+						Name:     "test fact",
+						Gatherer: "factone",
+						Argument: "arg",
+					},
+				},
+			},
+		},
+	}
+
+	data, err := anypb.New(&event)
+
+	assert.NoError(t, err)
+	ce := events.CloudEvent{
+		Id:          "id",
+		Source:      "wandalorian",
+		SpecVersion: "1.0",
+		Type:        string(event.ProtoReflect().Descriptor().FullName()),
+		Data: &events.CloudEvent_ProtoData{
+			ProtoData: data,
+		},
+	}
+
+	rawCe, err := proto.Marshal(&ce)
+	assert.NoError(t, err)
+	encodedEvent, err := events.ToEvent(&event, "wandalorian")
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, rawCe, encodedEvent)
+}
+
+func TestFromEvent(t *testing.T) {
+	event := events.FactsGatheringRequested{
+		ExecutionId: "exe",
+		GroupId:     "group_1",
+		Targets:     []string{"target1"},
+		Facts: []*events.AgentFact{
+			{
+				AgentId: "agent_1",
+				Facts: []*events.FactDefinition{
+					{
+						CheckId:  "check_1",
+						Name:     "test fact",
+						Gatherer: "factone",
+						Argument: "arg",
+					},
+				},
+			},
+		},
+	}
+
+	data, err := anypb.New(&event)
+
+	assert.NoError(t, err)
+	ce := events.CloudEvent{
+		Id:          "id",
+		Source:      "wandalorian",
+		SpecVersion: "1.0",
+		Type:        "FactsGatheringRequested",
+		Data: &events.CloudEvent_ProtoData{
+			ProtoData: data,
+		},
+	}
+
+	rawCe, err := proto.Marshal(&ce)
+	assert.NoError(t, err)
+
+	// decode test
+
+	var decodedEvent events.FactsGatheringRequested
+
+	err = events.FromEvent(rawCe, &decodedEvent)
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, event.GetExecutionId(), decodedEvent.GetExecutionId())
+	assert.EqualValues(t, event.GetGroupId(), decodedEvent.GetGroupId())
+	assert.EqualValues(t, event.GetTargets(), decodedEvent.GetTargets())
+	assert.EqualValues(t, event.Facts[0].String(), event.Facts[0].String())
+}
