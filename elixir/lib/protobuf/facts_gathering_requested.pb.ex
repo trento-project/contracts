d9d77cc7fba3dadf07e0a85ee0cc4c73b6b84b57
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-09-09 10:36:12 +0200

    Refactor facts gathering requested event

--- elixir/lib/protobuf/facts_gathering_requested.pb.ex
+++ elixir/lib/protobuf/facts_gathering_requested.pb.ex
@@ -1,4 +1,4 @@
-defmodule Trento.Checks.V1.FactDefinition do
+defmodule Trento.Checks.V1.FactRequest do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
@@ -8,12 +8,16 @@ defmodule Trento.Checks.V1.FactDefinition do
   field :argument, 4, type: :string
 end
 
-defmodule Trento.Checks.V1.AgentFact do
+defmodule Trento.Checks.V1.FactsGatheringRequestedTarget do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
   field :agent_id, 1, type: :string, json_name: "agentId"
-  field :facts, 2, repeated: true, type: Trento.Checks.V1.FactDefinition
+
+  field :fact_requests, 2,
+    repeated: true,
+    type: Trento.Checks.V1.FactRequest,
+    json_name: "factRequests"
 end
 
 defmodule Trento.Checks.V1.FactsGatheringRequested do
@@ -22,6 +26,5 @@ defmodule Trento.Checks.V1.FactsGatheringRequested do
 
   field :execution_id, 1, type: :string, json_name: "executionId"
   field :group_id, 2, type: :string, json_name: "groupId"
-  field :targets, 3, repeated: true, type: :string
-  field :facts, 4, repeated: true, type: Trento.Checks.V1.AgentFact
+  field :targets, 3, repeated: true, type: Trento.Checks.V1.FactsGatheringRequestedTarget
 end
