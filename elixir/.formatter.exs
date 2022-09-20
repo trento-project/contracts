8b05a73f6d31c5b9f4bff462b9f19b8b65e47948
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-09-20 10:39:18 +0200

    Exclude generated modules from formatter (#10)

--- elixir/.formatter.exs
+++ elixir/.formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter}.exs", "{config,test}/**/*.{ex,exs}", "lib/*.ex"],
   import_deps: [:protobuf]
 ]
