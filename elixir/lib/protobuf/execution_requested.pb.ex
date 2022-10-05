357519af9b32d2e96542f077914955aaf8033e08
Xabier Arbulu Insausti <xarbulu@suse.com>
2022-10-05 15:42:11 +0200

    Use a typed value for the ExecutionRequested env map (#23)

--- elixir/lib/protobuf/execution_requested.pb.ex
+++ elixir/lib/protobuf/execution_requested.pb.ex
@@ -11,7 +11,7 @@ defmodule Trento.Checks.V1.ExecutionRequested.EnvEntry do
   use Protobuf, map: true, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
   field :key, 1, type: :string
-  field :value, 2, type: :string
+  field :value, 2, type: Google.Protobuf.Value
 end
 
 defmodule Trento.Checks.V1.ExecutionRequested do
