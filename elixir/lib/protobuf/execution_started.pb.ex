1bff79a36cfc81b8fbb2166e38525f86cf223219
Carmine Di Monaco <carmine.dimonaco@suse.com>
2023-01-18 10:40:03 +0100

    Add ExecutionStarted event

---
+++ elixir/lib/protobuf/execution_started.pb.ex
@@ -0,0 +1,8 @@
+defmodule Trento.Checks.V1.ExecutionStarted do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :execution_id, 1, type: :string, json_name: "executionId"
+  field :group_id, 2, type: :string, json_name: "groupId"
+  field :targets, 3, repeated: true, type: Trento.Checks.V1.Target
+end
