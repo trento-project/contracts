53bce57d28b18fd4d73cab30a6e0793b8204139b
Nelson Kopliku <nelson.kopliku@suse.com>
2023-06-23 08:45:02 +0200

    Make target type a simple string

--- elixir/lib/protobuf/target_type.pb.ex
+++
@@ -1,9 +0,0 @@
-defmodule Trento.Checks.V1.TargetType do
-  @moduledoc false
-
-  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
-
-  field :TARGET_TYPE_UNSPECIFIED, 0
-  field :CLUSTER, 1
-  field :HOST, 2
-end
