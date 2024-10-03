36efbe7e650bb20e7de6a2ca96aac2a1b70536c9
arbulu89 <xarbulu@suse.com>
2024-10-03 10:38:24 +0200

    Add phase to operation execution completed message

--- elixir/lib/protobuf/operation_execution_completed.pb.ex
+++ elixir/lib/protobuf/operation_execution_completed.pb.ex
@@ -1,4 +1,4 @@
-defmodule Trento.Operations.V1.ErrorPhase do
+defmodule Trento.Operations.V1.OperationPhase do
   @moduledoc false
 
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
@@ -9,20 +9,7 @@ defmodule Trento.Operations.V1.ErrorPhase do
   field :ROLLBACK, 3
 end
 
-defmodule Trento.Operations.V1.OperationReportError do
-  @moduledoc false
-
-  use Protobuf, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
-
-  field :message, 1, type: :string
-
-  field :error_phase, 2,
-    type: Trento.Operations.V1.ErrorPhase,
-    json_name: "errorPhase",
-    enum: true
-end
-
-defmodule Trento.Operations.V1.OperationReportResponse.DiffEntry do
+defmodule Trento.Operations.V1.OperationResponse.DiffEntry do
   @moduledoc false
 
   use Protobuf, map: true, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
@@ -31,20 +18,33 @@ defmodule Trento.Operations.V1.OperationReportResponse.DiffEntry do
   field :value, 2, type: :string
 end
 
-defmodule Trento.Operations.V1.OperationReportResponse do
+defmodule Trento.Operations.V1.OperationResponse do
   @moduledoc false
 
   use Protobuf, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
 
-  field :result, 1, type: Trento.Operations.V1.OperationResult, enum: true
+  field :phase, 1, type: Trento.Operations.V1.OperationPhase, enum: true
 
   field :diff, 2,
     repeated: true,
-    type: Trento.Operations.V1.OperationReportResponse.DiffEntry,
+    type: Trento.Operations.V1.OperationResponse.DiffEntry,
     map: true
 end
 
-defmodule Trento.Operations.V1.OperationExecutionReport do
+defmodule Trento.Operations.V1.OperationError do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
+
+  field :error_phase, 1,
+    type: Trento.Operations.V1.OperationPhase,
+    json_name: "errorPhase",
+    enum: true
+
+  field :message, 2, type: :string
+end
+
+defmodule Trento.Operations.V1.OperationExecutionCompleted do
   @moduledoc false
 
   use Protobuf, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
@@ -55,10 +55,10 @@ defmodule Trento.Operations.V1.OperationExecutionReport do
   field :group_id, 2, type: :string, json_name: "groupId"
   field :step_number, 3, type: :int32, json_name: "stepNumber"
   field :agent_id, 4, type: :string, json_name: "agentId"
-  field :response, 5, type: Trento.Operations.V1.OperationReportResponse, oneof: 0
+  field :response, 5, type: Trento.Operations.V1.OperationResponse, oneof: 0
 
   field :error_value, 6,
-    type: Trento.Operations.V1.OperationReportError,
+    type: Trento.Operations.V1.OperationError,
     json_name: "errorValue",
     oneof: 0
 end
