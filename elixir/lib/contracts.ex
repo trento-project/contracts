21b9463050907fdf7a41844fb1a1d9c70118a554
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2024-11-06 16:40:42 +0100

    Elixir contracts signing and validation checking

--- elixir/lib/contracts.ex
+++ elixir/lib/contracts.ex
@@ -5,6 +5,8 @@ defmodule Trento.Contracts do
 
   require Logger
 
+  @default_event_validity 20
+
   @doc """
   Return the content type of contracts
   """
@@ -14,33 +16,23 @@ defmodule Trento.Contracts do
   Encode and wrap an event struct to a protobuf CloudEvent.
   """
   @spec to_event(struct(), Keyword.t()) :: binary()
-  def to_event(%mod{} = struct, opts \\ []) do
-    id = Keyword.get(opts, :id, UUID.uuid4())
-    source = Keyword.get(opts, :source, "trento")
-    private_key = Keyword.get(opts, :private_key, "")
+  def to_event(%{} = struct, opts \\ []) do
+    struct
+    |> build_cloud_event(opts)
+    |> CloudEvents.CloudEvent.encode()
+  end
 
-    time =
-      Keyword.get(
-        opts,
-        :time,
-        DateTime.utc_now()
-      )
-
-    time_attr = %Google.Protobuf.Timestamp{seconds: time |> DateTime.to_unix()}
-    data = Protobuf.Encoder.encode(struct)
-
-    cloud_event = %CloudEvents.CloudEvent{
-      data: {:proto_data, %Google.Protobuf.Any{value: data, type_url: get_type(mod)}},
-      spec_version: "1.0",
-      type: get_type(mod),
-      id: id,
-      attributes: %{
-        "time" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_timestamp, time_attr}},
-      },
-      source: source
-    } |> add_signature(private_key)
-
-    CloudEvents.CloudEvent.encode(cloud_event)
+  @doc """
+  Encode and wrap an event struct to a signed protobuf Cloudevent.
+  Accepts the private key as a pem file content.
+  Accepts the same options as to_event/2
+  """
+  @spec to_signed_event(struct(), binary(), Keyword.t()) :: binary()
+  def to_signed_event(struct, pem_private_key, opts \\ []) do
+    struct
+    |> build_cloud_event(opts)
+    |> add_signature(pem_private_key)
+    |> CloudEvents.CloudEvent.encode()
   end
 
   @doc """
@@ -50,12 +42,107 @@ defmodule Trento.Contracts do
           {:ok, struct()}
           | {:error, :decoding_error}
           | {:error, :invalid_envelope}
-          | {:error, :event_not_found}
+          | {:error, :unknown_event}
+          | {:error, :event_expired}
   def from_event(value) do
+    with {:ok, type, cloud_event} <- decode_cloud_event(value),
+         {:ok, cloud_event} <- verify_event_validity(cloud_event) do
+      decode_trento_event(type, cloud_event)
+    end
+  end
+
+  @doc """
+  Decode and unwrap a signed protobuf CloudEvent to an event struct.
+  """
+  @spec from_signed_event(binary(), binary()) ::
+          {:ok, struct()}
+          | {:error, :decoding_error}
+          | {:error, :invalid_envelope}
+          | {:error, :unknown_event}
+          | {:error, :event_expired}
+          | {:error, :invalid_event_signature}
+  def from_signed_event(value, public_key) do
+    with {:ok, event_type, event_data} <- decode_cloud_event(value),
+         {:ok, event_data} <- verify_event_signature(event_data, public_key),
+         {:ok, event_data} <- verify_event_validity(event_data) do
+      decode_trento_event(event_type, event_data)
+    end
+  end
+
+  defp build_cloud_event(%mod{} = struct, opts) do
+    id = Keyword.get(opts, :id, UUID.uuid4())
+    source = Keyword.get(opts, :source, "trento")
+    validity_in_seconds = Keyword.get(opts, :validity_in_seconds, @default_event_validity)
+
+    time =
+      Keyword.get(
+        opts,
+        :time,
+        DateTime.utc_now()
+      )
+
+    expiration = DateTime.add(time, validity_in_seconds)
+
+    time_attr = %Google.Protobuf.Timestamp{seconds: time |> DateTime.to_unix()}
+    expire_at_attr = %Google.Protobuf.Timestamp{seconds: expiration |> DateTime.to_unix()}
+
+    data = Protobuf.Encoder.encode(struct)
+
+    %CloudEvents.CloudEvent{
+      data: {:proto_data, %Google.Protobuf.Any{value: data, type_url: get_type(mod)}},
+      spec_version: "1.0",
+      type: get_type(mod),
+      id: id,
+      attributes: %{
+        "time" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_timestamp, time_attr}},
+        "expire_at" => %CloudEvents.CloudEventAttributeValue{
+          attr: {:ce_timestamp, expire_at_attr}
+        }
+      },
+      source: source
+    }
+  end
+
+  defp add_signature(
+         %CloudEvents.CloudEvent{
+           data: {:proto_data, %Google.Protobuf.Any{value: data}},
+           attributes:
+             %{
+               "time" => %CloudEvents.CloudEventAttributeValue{
+                 attr: {:ce_timestamp, %{seconds: time}}
+               },
+               "expire_at" => %CloudEvents.CloudEventAttributeValue{
+                 attr: {:ce_timestamp, %{seconds: expire_at}}
+               }
+             } = current_attrs
+         } = event,
+         private_key_content
+       ) do
+    signing_key =
+      private_key_content
+      |> :public_key.pem_decode()
+      |> Enum.at(0)
+      |> :public_key.pem_entry_decode()
+
+    time_str = Integer.to_string(time)
+    signature = :public_key.sign("#{data}#{time_str}#{expire_at}", :sha256, signing_key)
+
+    %CloudEvents.CloudEvent{
+      event
+      | attributes:
+          Map.put(
+            current_attrs,
+            "signature",
+            %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+          )
+    }
+  end
+
+  defp decode_cloud_event(data) do
     try do
-      case CloudEvents.CloudEvent.decode(value) do
-        %{type: type, data: {:proto_data, %Google.Protobuf.Any{value: data}}} ->
-          decode(type, data)
+      case CloudEvents.CloudEvent.decode(data) do
+        %{type: type, data: {:proto_data, %Google.Protobuf.Any{value: _data}}} = cloud_event ->
+          {:ok, type, cloud_event}
 
         event ->
           Logger.error("Invalid cloud event: #{inspect(event)}")
@@ -70,7 +157,63 @@ defmodule Trento.Contracts do
     end
   end
 
-  defp decode(type, data) do
+  defp verify_event_signature(
+         %CloudEvents.CloudEvent{
+           data: {:proto_data, %Google.Protobuf.Any{value: data}},
+           attributes: %{
+             "time" => %CloudEvents.CloudEventAttributeValue{
+               attr: {:ce_timestamp, %{seconds: time}}
+             },
+             "expire_at" => %CloudEvents.CloudEventAttributeValue{
+               attr: {:ce_timestamp, %{seconds: expire_at}}
+             },
+             "signature" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+           }
+         } = event,
+         public_key
+       ) do
+    signing_key =
+      public_key
+      |> :public_key.pem_decode()
+      |> Enum.at(0)
+      |> :public_key.pem_entry_decode()
+
+    event_signature_valid? =
+      :public_key.verify("#{data}#{time}#{expire_at}", :sha256, signature, signing_key)
+
+    if event_signature_valid? do
+      {:ok, event}
+    else
+      {:error, :invalid_event_signature}
+    end
+  end
+
+  defp verify_event_signature(_, _), do: {:error, :invalid_event_signature}
+
+  defp verify_event_validity(
+         %CloudEvents.CloudEvent{
+           attributes: %{
+             "expire_at" => %CloudEvents.CloudEventAttributeValue{
+               attr: {:ce_timestamp, %{seconds: expire_at_ts}}
+             }
+           }
+         } = event
+       ) do
+    expire_at = DateTime.from_unix!(expire_at_ts)
+    event_valid? = DateTime.compare(DateTime.utc_now(), expire_at) == :lt
+
+    if event_valid? do
+      {:ok, event}
+    else
+      {:error, :event_expired}
+    end
+  end
+
+  defp verify_event_validity(_), do: {:error, :event_expired}
+
+  defp decode_trento_event(type, %CloudEvents.CloudEvent{
+         data: {:proto_data, %Google.Protobuf.Any{value: data}}
+       }) do
     try do
       module_name = Macro.camelize(type)
       module = Module.safe_concat([module_name])
@@ -81,58 +224,11 @@ defmodule Trento.Contracts do
     end
   end
 
+  defp decode_trento_event(_, _), do: {:error, :unknown_event}
+
   defp get_type(mod) do
     mod
     |> Atom.to_string()
     |> String.replace("Elixir.", "")
   end
-
-  defp add_signature(event, ""), do: event
-
-  defp add_signature(
-    %CloudEvents.CloudEvent{
-      data: {:proto_data, %Google.Protobuf.Any{value: data}},
-      attributes: %{
-        "time" => %CloudEvents.CloudEventAttributeValue{
-          attr: {:ce_timestamp, %{seconds: time}}
-        }
-      } = current_attrs
-    } = event, private_key) do
-
-    time_str = Integer.to_string(time)
-    signature = :public_key.sign(data <> time_str, :sha256, private_key)
-
-    %CloudEvents.CloudEvent{
-      event |
-      attributes: Map.put(
-        current_attrs,
-        "signature",
-        %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
-      )
-    }
-  end
-
-  # def add_signature(event, private_key) do
-  #   decoded_event = %{
-  #     data: {:proto_data, %Google.Protobuf.Any{value: data}},
-  #     attributes: %{
-  #       "time" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_timestamp, time}}
-  #     } = current_attrs
-  #   } = CloudEvents.CloudEvent.decode(event)
-
-  #   %{seconds: seconds} = time
-  #   time_str = Integer.to_string(seconds)
-  #   signature = :public_key.sign(data <> time_str, :sha256, private_key)
-
-  #   updated_event = %CloudEvents.CloudEvent{
-  #     decoded_event |
-  #     attributes: Map.put(
-  #       current_attrs,
-  #       "signature",
-  #       %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
-  #     )
-  #   }
-
-  #   CloudEvents.CloudEvent.encode(updated_event)
-  # end
 end
--- elixir/test/contracts_test.exs
+++ elixir/test/contracts_test.exs
@@ -3,90 +3,417 @@ defmodule Trento.ContractsTest do
 
   alias CloudEvents.CloudEvent
 
-  test "should decode to the right struct" do
-    event_id = UUID.uuid4()
-    event = %Test.Event{id: event_id}
+  describe "message with signature" do
+    setup do
+      private_key = File.read!("test/support/fixtures/test.private.pem")
+      public_key = File.read!("test/support/fixtures/test.public.pem")
 
-    cloudevent = %CloudEvent{
-      data:
-        {:proto_data,
-         %Google.Protobuf.Any{
-           __unknown_fields__: [],
-           type_url: "test.Event",
-           value: Test.Event.encode(event)
-         }},
-      id: UUID.uuid4(),
-      source: "wandalorian",
-      spec_version: "1.0",
-      type: "test.Event"
-    }
+      %{private_key: private_key, public_key: public_key}
+    end
 
-    encoded_cloudevent = CloudEvent.encode(cloudevent)
+    test "should decode to the right struct", %{public_key: public_key, private_key: private_key} do
+      event_id = UUID.uuid4()
+      event = %Test.Event{id: event_id}
 
-    assert {:ok, %Test.Event{id: ^event_id}} = Trento.Contracts.from_event(encoded_cloudevent)
-  end
+      time = DateTime.utc_now()
+      time_ts = DateTime.to_unix(time)
 
-  test "should encode to the right struct" do
-    event = %Test.Event{id: UUID.uuid4()}
-    cloudevent_id = UUID.uuid4()
-    time = DateTime.utc_now()
-    time_attr = %Google.Protobuf.Timestamp{seconds: time |> DateTime.to_unix()}
+      expire_at = DateTime.add(time, 60)
+      expire_at_ts = DateTime.to_unix(expire_at)
 
-    cloudevent = %CloudEvent{
-      data:
-        {:proto_data,
-         %Google.Protobuf.Any{
-           __unknown_fields__: [],
-           type_url: "Test.Event",
-           value: Test.Event.encode(event)
-         }},
-      id: cloudevent_id,
-      source: "wandalorian",
-      spec_version: "1.0",
-      type: "Test.Event",
-      attributes: %{
-        "time" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_timestamp, time_attr}}
+      message_content = Test.Event.encode(event)
+
+      signing_key =
+        private_key
+        |> :public_key.pem_decode()
+        |> Enum.at(0)
+        |> :public_key.pem_entry_decode()
+
+      signature =
+        :public_key.sign("#{message_content}#{time_ts}#{expire_at_ts}", :sha256, signing_key)
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "test.Event",
+             value: message_content
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: expire_at_ts}}
+          },
+          "time" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: time_ts}}
+          },
+          "signature" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "test.Event"
       }
-    }
 
-    encoded_cloudevent = CloudEvent.encode(cloudevent)
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
 
-    assert encoded_cloudevent ==
-             Trento.Contracts.to_event(event,
-               id: cloudevent_id,
-               source: "wandalorian",
-               time: time
-             )
+      assert {:ok, %Test.Event{id: ^event_id}} =
+               Trento.Contracts.from_signed_event(encoded_cloudevent, public_key)
+    end
+
+    test "should encode to the right struct", %{private_key: private_key} do
+      event_id = UUID.uuid4()
+      event = %Test.Event{id: event_id}
+
+      time = DateTime.utc_now()
+      time_ts = DateTime.to_unix(time)
+
+      expire_at = DateTime.add(time, 20)
+      expire_at_ts = DateTime.to_unix(expire_at)
+
+      message_content = Test.Event.encode(event)
+
+      signing_key =
+        private_key
+        |> :public_key.pem_decode()
+        |> Enum.at(0)
+        |> :public_key.pem_entry_decode()
+
+      signature =
+        :public_key.sign("#{message_content}#{time_ts}#{expire_at_ts}", :sha256, signing_key)
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "Test.Event",
+             value: message_content
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: expire_at_ts}}
+          },
+          "time" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: time_ts}}
+          },
+          "signature" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+        },
+        id: event_id,
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "Test.Event"
+      }
+
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
+
+      assert encoded_cloudevent ==
+               Trento.Contracts.to_signed_event(
+                 event,
+                 private_key,
+                 id: event_id,
+                 source: "wandalorian",
+                 time: time
+               )
+    end
+
+    test "should return error if the event is not wrapped in a CloudEvent", %{
+      public_key: public_key
+    } do
+      event = Test.Event.encode(%Test.Event{id: UUID.uuid4()})
+
+      assert {:error, :invalid_envelope} = Trento.Contracts.from_signed_event(event, public_key)
+    end
+
+    test "should return error if the could not be decoded", %{
+      public_key: public_key
+    } do
+      event = <<0, 0>>
+
+      assert {:error, :decoding_error} = Trento.Contracts.from_signed_event(event, public_key)
+    end
+
+    test "should return error if the event type is unknown", %{
+      private_key: private_key,
+      public_key: public_key
+    } do
+      time = DateTime.utc_now()
+      time_ts = DateTime.to_unix(time)
+
+      expire_at = DateTime.add(time, 60)
+      expire_at_ts = DateTime.to_unix(expire_at)
+
+      message_content = <<0, 0, 0, 0, 0, 0, 0, 0>>
+
+      signing_key =
+        private_key
+        |> :public_key.pem_decode()
+        |> Enum.at(0)
+        |> :public_key.pem_entry_decode()
+
+      signature =
+        :public_key.sign("#{message_content}#{time_ts}#{expire_at_ts}", :sha256, signing_key)
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "unknown.Event",
+             value: message_content
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: expire_at_ts}}
+          },
+          "time" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: time_ts}}
+          },
+          "signature" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "unknown.Event"
+      }
+
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
+
+      assert {:error, :unknown_event} =
+               Trento.Contracts.from_signed_event(encoded_cloudevent, public_key)
+    end
+
+    test "should return error if the event is expired", %{
+      private_key: private_key,
+      public_key: public_key
+    } do
+      event_id = UUID.uuid4()
+      event = %Test.Event{id: event_id}
+
+      time = DateTime.utc_now()
+      time_ts = DateTime.to_unix(time)
+
+      expire_at = DateTime.add(time, -60)
+      expire_at_ts = DateTime.to_unix(expire_at)
+
+      message_content = Test.Event.encode(event)
+
+      signing_key =
+        private_key
+        |> :public_key.pem_decode()
+        |> Enum.at(0)
+        |> :public_key.pem_entry_decode()
+
+      signature =
+        :public_key.sign("#{message_content}#{time_ts}#{expire_at_ts}", :sha256, signing_key)
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "test.Event",
+             value: message_content
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: expire_at_ts}}
+          },
+          "time" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: time_ts}}
+          },
+          "signature" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "test.Event"
+      }
+
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
+
+      assert {:error, :event_expired} =
+               Trento.Contracts.from_signed_event(encoded_cloudevent, public_key)
+    end
+
+    test "should return error if the event signature is not valid", %{
+      public_key: public_key
+    } do
+      event_id = UUID.uuid4()
+      event = %Test.Event{id: event_id}
+
+      time = DateTime.utc_now()
+      time_ts = DateTime.to_unix(time)
+
+      expire_at = DateTime.add(time, -60)
+      expire_at_ts = DateTime.to_unix(expire_at)
+
+      message_content = Test.Event.encode(event)
+      signature = "invalidsignature"
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "test.Event",
+             value: message_content
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: expire_at_ts}}
+          },
+          "time" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: time_ts}}
+          },
+          "signature" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_bytes, signature}}
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "test.Event"
+      }
+
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
+
+      assert {:error, :invalid_event_signature} =
+               Trento.Contracts.from_signed_event(encoded_cloudevent, public_key)
+    end
   end
 
-  test "should return error if the event is not wrapped in a CloudEvent" do
-    event = Test.Event.encode(%Test.Event{id: UUID.uuid4()})
+  describe "message without signature" do
+    test "should decode to the right struct" do
+      event_id = UUID.uuid4()
+      event = %Test.Event{id: event_id}
+      time = DateTime.utc_now()
 
-    assert {:error, :invalid_envelope} = Trento.Contracts.from_event(event)
-  end
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "test.Event",
+             value: Test.Event.encode(event)
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: DateTime.add(time, 60) |> DateTime.to_unix()}}
+          }
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "test.Event"
+      }
 
-  test "should return error if the could not be decoded" do
-    event = <<0, 0>>
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
 
-    assert {:error, :decoding_error} = Trento.Contracts.from_event(event)
-  end
+      assert {:ok, %Test.Event{id: ^event_id}} = Trento.Contracts.from_event(encoded_cloudevent)
+    end
 
-  test "should return error if the event type is unknown" do
-    cloudevent = %CloudEvent{
-      data:
-        {:proto_data,
-         %Google.Protobuf.Any{
-           __unknown_fields__: [],
-           type_url: "Unknown.Event",
-           value: <<0, 0, 0, 0, 0, 0, 0, 0>>
-         }},
-      id: UUID.uuid4(),
-      source: "wandalorian",
-      spec_version: "1.0",
-      type: "Unknown.Event"
-    }
+    test "should encode to the right struct" do
+      event = %Test.Event{id: UUID.uuid4()}
+      cloudevent_id = UUID.uuid4()
+      time = DateTime.utc_now()
+      time_attr = %Google.Protobuf.Timestamp{seconds: time |> DateTime.to_unix()}
 
-    assert {:error, :unknown_event} =
-             cloudevent |> CloudEvent.encode() |> Trento.Contracts.from_event()
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "Test.Event",
+             value: Test.Event.encode(event)
+           }},
+        id: cloudevent_id,
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "Test.Event",
+        attributes: %{
+          "time" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_timestamp, time_attr}},
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: DateTime.add(time, 20) |> DateTime.to_unix()}}
+          }
+        }
+      }
+
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
+
+      assert encoded_cloudevent ==
+               Trento.Contracts.to_event(event,
+                 id: cloudevent_id,
+                 source: "wandalorian",
+                 time: time
+               )
+    end
+
+    test "should return error if the event is not wrapped in a CloudEvent" do
+      event = Test.Event.encode(%Test.Event{id: UUID.uuid4()})
+
+      assert {:error, :invalid_envelope} = Trento.Contracts.from_event(event)
+    end
+
+    test "should return error if the could not be decoded" do
+      event = <<0, 0>>
+
+      assert {:error, :decoding_error} = Trento.Contracts.from_event(event)
+    end
+
+    test "should return error if the event type is unknown" do
+      time = DateTime.utc_now()
+      time_attr = %Google.Protobuf.Timestamp{seconds: time |> DateTime.to_unix()}
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "Unknown.Event",
+             value: <<0, 0, 0, 0, 0, 0, 0, 0>>
+           }},
+        attributes: %{
+          "time" => %CloudEvents.CloudEventAttributeValue{attr: {:ce_timestamp, time_attr}},
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: DateTime.add(time, 20) |> DateTime.to_unix()}}
+          }
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "Unknown.Event"
+      }
+
+      assert {:error, :unknown_event} =
+               cloudevent |> CloudEvent.encode() |> Trento.Contracts.from_event()
+    end
+
+    test "should return error if the event is expired" do
+      event_id = UUID.uuid4()
+      event = %Test.Event{id: event_id}
+      time = DateTime.utc_now()
+
+      cloudevent = %CloudEvent{
+        data:
+          {:proto_data,
+           %Google.Protobuf.Any{
+             __unknown_fields__: [],
+             type_url: "test.Event",
+             value: Test.Event.encode(event)
+           }},
+        attributes: %{
+          "expire_at" => %CloudEvents.CloudEventAttributeValue{
+            attr: {:ce_timestamp, %{seconds: DateTime.add(time, -10) |> DateTime.to_unix()}}
+          }
+        },
+        id: UUID.uuid4(),
+        source: "wandalorian",
+        spec_version: "1.0",
+        type: "test.Event"
+      }
+
+      encoded_cloudevent = CloudEvent.encode(cloudevent)
+
+      assert {:error, :event_expired} = Trento.Contracts.from_event(encoded_cloudevent)
+    end
   end
 end
