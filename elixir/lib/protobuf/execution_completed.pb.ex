9ee0e42035b4bf5668ad095ff1edfec7b4f56683
arbulu89 <xarbulu@suse.com>
2025-01-07 15:41:57 +0100

    Update elixir autogenerated files

--- elixir/lib/protobuf/execution_completed.pb.ex
+++ elixir/lib/protobuf/execution_completed.pb.ex
@@ -1,7 +1,7 @@
 defmodule Trento.Checks.V1.Result do
   @moduledoc false
 
-  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+  use Protobuf, enum: true, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
 
   field :PASSING, 0
   field :WARNING, 1
@@ -11,7 +11,7 @@ end
 defmodule Trento.Checks.V1.ExecutionCompleted do
   @moduledoc false
 
-  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
 
   field :execution_id, 1, type: :string, json_name: "executionId"
   field :group_id, 2, type: :string, json_name: "groupId"
