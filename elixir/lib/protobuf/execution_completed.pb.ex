00253288eb677190dbc4350be4c94c415c0342ba
arbulu89 <xarbulu@suse.com>
2025-02-17 10:17:26 +0100

    Update elixir protobuf to 0.14.0

--- elixir/lib/protobuf/execution_completed.pb.ex
+++ elixir/lib/protobuf/execution_completed.pb.ex
@@ -1,7 +1,7 @@
 defmodule Trento.Checks.V1.Result do
   @moduledoc false
 
-  use Protobuf, enum: true, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.14.0", syntax: :proto3
 
   field :PASSING, 0
   field :WARNING, 1
@@ -11,7 +11,7 @@ end
 defmodule Trento.Checks.V1.ExecutionCompleted do
   @moduledoc false
 
-  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+  use Protobuf, protoc_gen_elixir_version: "0.14.0", syntax: :proto3
 
   field :execution_id, 1, type: :string, json_name: "executionId"
   field :group_id, 2, type: :string, json_name: "groupId"
