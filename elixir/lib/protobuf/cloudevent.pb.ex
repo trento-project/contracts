b18ea7134964bef7497174ac6e66dc3a82328e70
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2025-02-26 15:37:21 +0100

    regenerate elixir stubs

--- elixir/lib/protobuf/cloudevent.pb.ex
+++ elixir/lib/protobuf/cloudevent.pb.ex
@@ -1,7 +1,7 @@
 defmodule CloudEvents.CloudEvent.AttributesEntry do
   @moduledoc false
 
-  use Protobuf, map: true, protoc_gen_elixir_version: "0.14.0", syntax: :proto3
+  use Protobuf, map: true, protoc_gen_elixir_version: "0.14.1", syntax: :proto3
 
   field :key, 1, type: :string
   field :value, 2, type: CloudEvents.CloudEventAttributeValue
@@ -10,7 +10,7 @@ end
 defmodule CloudEvents.CloudEvent do
   @moduledoc false
 
-  use Protobuf, protoc_gen_elixir_version: "0.14.0", syntax: :proto3
+  use Protobuf, protoc_gen_elixir_version: "0.14.1", syntax: :proto3
 
   oneof :data, 0
 
@@ -27,7 +27,7 @@ end
 defmodule CloudEvents.CloudEventAttributeValue do
   @moduledoc false
 
-  use Protobuf, protoc_gen_elixir_version: "0.14.0", syntax: :proto3
+  use Protobuf, protoc_gen_elixir_version: "0.14.1", syntax: :proto3
 
   oneof :attr, 0
 
