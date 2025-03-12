524d69b6823fd419c93ce145bf51cd5cc83c698f
Nelson Kopliku <nelson.kopliku@suse.com>
2025-03-12 10:45:23 +0100

    Remove original value from customization message (#93)

--- elixir/lib/protobuf/check_custom_value.pb.ex
+++ elixir/lib/protobuf/check_custom_value.pb.ex
@@ -5,13 +5,8 @@ defmodule Trento.Checks.V1.CheckCustomValue do
 
   oneof :value, 0
 
-  oneof :original_value, 1
-
   field :name, 1, type: :string
   field :string_value, 2, type: :string, json_name: "stringValue", oneof: 0
   field :int_value, 3, type: :int32, json_name: "intValue", oneof: 0
   field :bool_value, 4, type: :bool, json_name: "boolValue", oneof: 0
-  field :string_original_value, 5, type: :string, json_name: "stringOriginalValue", oneof: 1
-  field :int_original_value, 6, type: :int32, json_name: "intOriginalValue", oneof: 1
-  field :bool_original_value, 7, type: :bool, json_name: "boolOriginalValue", oneof: 1
 end
