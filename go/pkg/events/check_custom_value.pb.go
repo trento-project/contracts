9222f8feea7fbc9d2c949919599fe8c881601184
Nelson Kopliku <nelson.kopliku@suse.com>
2025-04-03 09:07:11 +0200

    Regenerate contracts

--- go/pkg/events/check_custom_value.pb.go
+++ go/pkg/events/check_custom_value.pb.go
@@ -27,6 +27,7 @@ type CheckCustomValue struct {
 
 	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
 	// Types that are assignable to Value:
+	//
 	//	*CheckCustomValue_StringValue
 	//	*CheckCustomValue_IntValue
 	//	*CheckCustomValue_BoolValue
